3f67c2f4cec7709faf10693497aa1e2673e5ddb5
Talha <mtalhamanzoor786@gmail.com>
2024-03-25 11:00:54 -0700

    Fixed some basic UI problems

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -5,11 +5,11 @@ const NewsCard = (props) => {
 
     return (
         <>
-            <div className="card text-bg-info" style={{ width: "18rem" }}>
+            <div className="card text-bg-info">
                 <img src={props.imgURL} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">{props.text}</p>
+                    <h5 className="card-title">{props.title.slice(0,45)+'...'}</h5>
+                    <p className="card-text">{props.text.slice(0,95)+'...'}</p>
                     <a href={props.url} className="btn btn-primary">Read More</a>
                 </div>
             </div>
