3d76c89d200cba896e6691996de9d74fd8ae9b02
Talha <mtalhamanzoor786@gmail.com>
2024-03-26 11:00:12 -0700

    Adde Prev and Next buttons successfully

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,9 +3,10 @@ import NewsCard from './NewsCard'
 
 const News = () => {
     const [data, setData] = useState([])
-
-    let fetchData = async () => {
-    let url="https://newsapi.org/v2/top-headlines?country=us&apiKey=0dd418eda768429cba391f6cab5450d9"
+    const [page, setPage] = useState(1)
+    let pageSize=5;
+    let fetchData = async (url) => {
+    // let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=0dd418eda768429cba391f6cab5450d9&&${page}`
         console.log("Fetching data...");
         let response = await fetch(url);
         let data = await response.json();
@@ -14,30 +15,42 @@ const News = () => {
     }
 
     useEffect(() => {
-        fetchData()
-    }, [])
-
-    const loadMore=()=>{
-        fetchData();
+        fetchData(`https://newsapi.org/v2/top-headlines?country=us&apiKey=0dd418eda768429cba391f6cab5450d9&page=${page}&pageSize=${5}`);
+    },[])
+    
+    const handlePrev=()=>{
+        setPage(page-1)
+        fetchData(`https://newsapi.org/v2/top-headlines?country=us&apiKey=0dd418eda768429cba391f6cab5450d9&page=${page}&pageSize=${5}`);
+        console.log('handle prev was clicked'); 
+    }
+    const handleNext=()=>{
+        setPage(page+1)
+        fetchData(`https://newsapi.org/v2/top-headlines?country=us&apiKey=0dd418eda768429cba391f6cab5450d9&page=${page}&pageSize=${5}`);
+        console.log('handle Next was clicked'); 
     }
     const techyImgURL = 'https://th.bing.com/th/id/R.1510fe8202711595e506301e5deba565?rik=cTQudyXDcx472w&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fc%2f1%2f8%2f647485.jpg&ehk=0RYQAaVD4Xiwm24kBigDwRxT1QpdyXVfPzyaANl%2bQGg%3d&risl=&pid=ImgRaw&r=0'
     return (
         <>
             <div className="container">
                 <div className="row">
+                    
                     {
                         data.map((elem) => {
                             if (elem.title !== "[Removed]" && elem.description) {
                                 return (
                                     <div key={elem.url} className="col-md-3">
                                         <NewsCard imgURL={elem.urlToImage ? elem.urlToImage : techyImgURL} title={elem.title} text={elem.description} url={elem.url} />
-                                        {console.log(elem.content)}
+                                        {/* {console.log(elem.content)} */}
                                     </div>
                                 );
                             }
                             return null; // If id is null or description is missing, return null
                         })
                     }
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button type="button" className='btn btn-dark' onClick={handlePrev} disabled={page<=1}>&larr; Prev</button>
+                    <button type="button" className='btn btn-dark' onClick={handleNext}>Next &rarr;</button>
                 </div>
             </div >
         </>
